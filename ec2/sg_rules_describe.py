8dbe64fb9adc9c27caa58859cc9af2ca56749062
Nikhil Maheshwari <36232275+nikhilbagdi@users.noreply.github.com>
2023-05-17 01:28:13 +0530

    updated comments in sg_rules_describe.py

--- ec2/sg_rules_describe.py
+++ ec2/sg_rules_describe.py
@@ -4,49 +4,63 @@ import csv
 import sys
 import pandas as pd
 
-env=str(sys.argv[1])
+# Get the environment argument from the command line
+env = str(sys.argv[1])
+
+# Set the AWS profile environment variable
 os.environ['AWS_PROFILE'] = env
 
-regions = ['us-east-1','ap-southeast-1','ap-south-1']
+# List of regions to iterate over
+regions = ['us-east-1', 'ap-southeast-1', 'ap-south-1']
 
-ec2=boto3.client('ec2',region_name=region)
+# Create an EC2 client
+ec2 = boto3.client('ec2')
+
+# List of security group IDs to retrieve information for
 sg_id = ['']
 
-sg=ec2.describe_security_groups(GroupIds=sg_id)
+# Describe security groups for the given security group IDs
+sg = ec2.describe_security_groups(GroupIds=sg_id)
 
-rulelist=[]
+# List to store security group rule information
+rulelist = []
 
+# Iterate over each security group
 for sg in sg['SecurityGroups']:
-    # print (sg['IpPermissions'])
     for ipPermission in sg['IpPermissions']:
-        # print (ipPermission)
-        # print ('\n')
         for permission in ipPermission['IpRanges']:
-            # print(permission)
-            ruledict={}
+            ruledict = {}
             if permission.get('Description') and ipPermission.get('FromPort'):
-                ruledict['GroupId']=sg['GroupId']
-                ruledict['Port']=ipPermission['FromPort']
-                ruledict['Protocol']=ipPermission['IpProtocol']
-                ruledict['CIDR']=permission['CidrIp']
-                ruledict['Description']=permission['Description']
-                # print (sg['GroupId'],ipPermission['FromPort'], ipPermission['IpProtocol'], permission['CidrIp'],permission['Description'])
+                # Store rule information for rules with description and from port
+                ruledict['GroupId'] = sg['GroupId']
+                ruledict['Port'] = ipPermission['FromPort']
+                ruledict['Protocol'] = ipPermission['IpProtocol']
+                ruledict['CIDR'] = permission['CidrIp']
+                ruledict['Description'] = permission['Description']
             elif ipPermission.get('FromPort') == '-1' or ipPermission.get('IpProtocol') == '-1':
-                ruledict['GroupId']=sg['GroupId']
-                ruledict['Port']='ALL'
-                ruledict['Protocol']='ALL'
-                ruledict['CIDR']=permission['CidrIp']
-                ruledict['Description']=permission['Description']
-            else :
-                ruledict['GroupId']=sg['GroupId']
-                ruledict['Port']=ipPermission['FromPort']
-                ruledict['Protocol']=ipPermission['IpProtocol']
-                ruledict['CIDR']=permission['CidrIp']
-                ruledict['Description']="--"
-                # print (sg['GroupId'],ipPermission['FromPort'], ipPermission['IpProtocol'], permission['CidrIp'],'--')
+                # Store rule information for rules with all ports and protocols
+                ruledict['GroupId'] = sg['GroupId']
+                ruledict['Port'] = 'ALL'
+                ruledict['Protocol'] = 'ALL'
+                ruledict['CIDR'] = permission['CidrIp']
+                ruledict['Description'] = permission['Description']
+            else:
+                # Store rule information for rules without description
+                ruledict['GroupId'] = sg['GroupId']
+                ruledict['Port'] = ipPermission['FromPort']
+                ruledict['Protocol'] = ipPermission['IpProtocol']
+                ruledict['CIDR'] = permission['CidrIp']
+                ruledict['Description'] = "--"
+            
+            # Append the dictionary to the list
             rulelist.append(ruledict)
 
+# Create a DataFrame from the list
 df = pd.DataFrame(rulelist)
+
+# Print the DataFrame
 print(df)
-filename="sg-inbound-rules.csv"
+
+# Export the DataFrame to a CSV file
+filename = "sg-inbound-rules.csv"
 df.to_csv(filename)
