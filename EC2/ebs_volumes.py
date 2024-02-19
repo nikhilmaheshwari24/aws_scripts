58cd220eb58084e479b0855fbc17b442ce8b4f69
Nikhil Maheshwari <36232275+nikhilmaheshwari24@users.noreply.github.com>
2024-02-19 15:59:41 +0000

    Updated ebsDescribe.py in EC2

--- EC2/ebs_volumes.py
+++ EC2/ebs_volumes.py
@@ -1,71 +1,82 @@
 import boto3
-import os
-import sys
-import csv
 from datetime import datetime, timezone
 import pandas as pd
+import os
 
-def age(date):
-    create_date = datetime.fromisoformat(str(date)).astimezone(timezone.utc)
+# Set AWS profile
+os.environ['AWS_PROFILE'] = "<profile-name>"
+
+def calculate_age(create_date):
+    """
+    Calculate the age of a volume based on its creation date.
+
+    Args:
+    create_date (datetime): The creation date of the volume.
+
+    Returns:
+    int: The age of the volume in days.
+    """
+    create_date = datetime.fromisoformat(str(create_date)).astimezone(timezone.utc)
     today = datetime.now(timezone.utc)
     age = today - create_date
     return age.days
 
-# regions
-regions=['us-east-2']
+# Regions
+regions = ['us-east-2']
 
-volumes_list=[]
+volumes_info = []
 
 for region in regions:
-
-    ebs_volumes = boto3.client('ec2', region_name=region)
-    paginator = ebs_volumes.get_paginator('describe_volumes')
+    ec2_client = boto3.client('ec2', region_name=region)
+    paginator = ec2_client.get_paginator('describe_volumes')
     volumes_paginator = paginator.paginate(PaginationConfig={'PageSize': 50})
 
     for page in volumes_paginator:
-
         for volume in page['Volumes']:
-            
-            print(volume['VolumeId']," <> ")
-
-            volume_dict={}
+            volume_details = {}
 
             # Extracting Volume Name
             if 'Tags' in volume:
                 for tag in volume['Tags']:
                     if 'Name' == tag['Key']:
-                        volume_dict['Name'] = tag['Value']
-                    else: 
+                        volume_details['Name'] = tag['Value']
+                    else:
                         continue
             else:
-               volume_dict['Name'] = ""
+                volume_details['Name'] = ""
 
-            volume_dict['CreateTime'] = volume['CreateTime']
-            volume_dict['VolumeAge'] = age(volume['CreateTime'])
-            volume_dict['VolumeID'] = volume['VolumeId']
-            volume_dict['State'] = volume['State']
-            volume_dict['VolumeType'] = volume['VolumeType']
-            volume_dict['AvailabilityZone'] = volume['AvailabilityZone']
-            volume_dict['Size'] = volume['Size']
-            volume_dict['Encryption'] = volume['Encrypted']
-            
+            volume_details['CreateTime'] = volume['CreateTime']
+            volume_details['VolumeAge'] = calculate_age(volume['CreateTime'])
+            volume_details['VolumeID'] = volume['VolumeId']
+            volume_details['State'] = volume['State']
+            volume_details['VolumeType'] = volume['VolumeType']
+            volume_details['AvailabilityZone'] = volume['AvailabilityZone']
+            volume_details['Size'] = volume['Size']
+            volume_details['Encryption'] = volume['Encrypted']
+
             if 'SnapshotId' in volume:
-                volume_dict['SnapshotId'] = volume['SnapshotId']
+                volume_details['SnapshotId'] = volume['SnapshotId']
             else:
-                volume_dict['SnapshotId'] = ""
+                volume_details['SnapshotId'] = ""
 
-            volume_dict['Iops'] = volume['Iops']
+            volume_details['Iops'] = volume['Iops']
 
             if 'Throughput' in volume:
-                volume_dict['Throughput'] = volume['Throughput']
+                volume_details['Throughput'] = volume['Throughput']
             else:
-                 volume_dict['Throughput'] = ""
+                volume_details['Throughput'] = ""
 
-            # volume_dict['FastRestored'] = volume['FastRestored']
-            # volume_dict['MultiAttachEnabled'] = volume['MultiAttachEnabled']
+            try:
+                tags = volume['Tags']
+                tag_list = [f"{tag['Key']}: {tag['Value']}" for tag in tags]
+                volume_details['Tags'] = '\n'.join(tag_list)
+            except:
+                volume_details['Tags'] = ""
 
-            volumes_list.append(volume_dict)
-            
-dataframe = pd.DataFrame(volumes_list)
+            volumes_info.append(volume_details)
+
+            print(volume['VolumeId'], " <> ")
+
+dataframe = pd.DataFrame(volumes_info)
 filename = "EBS_Volumes.csv"
-dataframe.to_csv(filename)
+dataframe.to_csv(filename, index=False)
