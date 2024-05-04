79a59d96cdb78e52ddd114fb75b6815ccca3a825
Nikhil Maheshwari <36232275+nikhilmaheshwari24@users.noreply.github.com>
2024-05-04 07:31:06 +0000

    added describe_instances script

--- EC2/describe_instances.py
+++ EC2/describe_instances.py
@@ -1,46 +1,59 @@
 import boto3
 import os
 import pandas as pd
+from datetime import datetime, timezone
 
 # Set AWS profile environment variable
-os.environ['AWS_PROFILE'] = ""
+os.environ['AWS_PROFILE'] = ''
 
-# Define AWS regions
+# Define AWS regions to search for instances
 regions = ['us-east-2']
 
+# Initialize a list to store instance information
 instances_list = []
 
-def instance_tags(instance_tags_list):
+# Define a directory to store CSV files
+directory = 'CSV'
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
+# Function to calculate age based on creation date
+def calculate_age(create_date):
+    create_date = datetime.fromisoformat(str(create_date)).astimezone(timezone.utc)
+    today = datetime.now(timezone.utc)
+    age = today - create_date
+    return age.days
+
+# Function to extract resource tags
+def resource_tags(resource_tags_list):
     # Initialize an empty list to store tags
     tag_list = []
-    
     # Process each tag and store it in the list
-    for tag in instance_tags_list:
+    for tag in resource_tags_list:
         key = tag['Key']
         value = tag['Value']
         tag_list.append(f"{key}: {value}")
-
     # Return the concatenated tags if tags are present
     if tag_list:
         return '\n'.join(tag_list)
     else:
         return "No Tags Available"
 
+# Iterate over each region
 for region in regions:
-
-    ec2_client = boto3.client('ec2',region_name=region)
+    # Create an EC2 client for the region
+    ec2_client = boto3.client('ec2', region_name=region)
+    # Describe instances
     paginator = ec2_client.get_paginator('describe_instances')
     ec2_paginator = paginator.paginate(PaginationConfig={'PageSize': 100})
-
+    # Iterate over each page of instances
     for page in ec2_paginator:
-
+        # Iterate over each reservation
         for reservation in page['Reservations']:
-
+            # Iterate over each instance
             for instance in reservation['Instances']:
-
                 instance_dict = {}
-
+                # Extract instance details
                 instance_dict['AmiLaunchIndex'] = instance['AmiLaunchIndex']
                 instance_dict['ImageId'] = instance['ImageId']
                 instance_dict['InstanceId'] = instance['InstanceId']
@@ -48,15 +61,14 @@ for region in regions:
                 instance_dict['KernelId'] = instance.get('KernelId', None)
                 instance_dict['KeyName'] = instance.get('KeyName', None)
                 instance_dict['LaunchTime'] = instance['LaunchTime']
+                instance_dict['Age'] = calculate_age(instance['LaunchTime'])
                 instance_dict['Monitoring'] = instance['Monitoring']['State']
-                # instance_dict['Placement'] = instance['Placement']
                 instance_dict['Placement_AvailabilityZone'] = instance['Placement']['AvailabilityZone']
                 instance_dict['Placement_Tenancy'] = instance['Placement']['Tenancy']
                 instance_dict['Platform'] = instance.get('Platform', None)
                 instance_dict['PrivateDnsName'] = instance['PrivateDnsName']
                 instance_dict['PrivateIpAddress'] = instance.get('PrivateIpAddress', None)
                 instance_dict['PrivateIpAddresses'] = instance.get('PrivateIpAddresses', {})
-
                 product_codes = instance.get('ProductCodes', [])
                 if product_codes:
                     instance_dict['ProductCodes_ProductCodeId'] = product_codes[0].get('ProductCodeId', None)
@@ -64,7 +76,6 @@ for region in regions:
                 else:
                     instance_dict['ProductCodes_ProductCodeId'] = None
                     instance_dict['ProductCodes_ProductCodeType'] = None
-
                 instance_dict['PublicDnsName'] = instance['PublicDnsName']
                 instance_dict['PublicIpAddress'] = instance.get('PublicIpAddress', None)
                 instance_dict['RamdiskId'] = instance.get('RamdiskId', None)
@@ -73,7 +84,7 @@ for region in regions:
                 instance_dict['SubnetId'] = instance.get('SubnetId', None)
                 instance_dict['VpcId'] = instance.get('VpcId', None)
                 instance_dict['Architecture'] = instance['Architecture']
-                instance_dict['BlockDeviceMappings'] = '\n'.join(map(str,instance['BlockDeviceMappings']))
+                instance_dict['BlockDeviceMappings'] = '\n'.join(map(str, instance['BlockDeviceMappings']))
                 instance_dict['ClientToken'] = instance['ClientToken']
                 instance_dict['EbsOptimized'] = instance['EbsOptimized']
                 instance_dict['EnaSupport'] = instance['EnaSupport']
@@ -82,16 +93,16 @@ for region in regions:
                 instance_dict['InstanceLifecycle'] = instance.get('InstanceLifecycle', None)
                 instance_dict['ElasticGpuAssociations'] = instance.get('ElasticGpuAssociations', {})
                 instance_dict['ElasticInferenceAcceleratorAssociations'] = instance.get('ElasticInferenceAcceleratorAssociations', [])
-                instance_dict['NetworkInterfaces'] = '\n'.join(map(str,instance['NetworkInterfaces']))
+                instance_dict['NetworkInterfaces'] = '\n'.join(map(str, instance['NetworkInterfaces']))
                 instance_dict['OutpostArn'] = instance.get('OutpostArn', None)
                 instance_dict['RootDeviceName'] = instance['RootDeviceName']
                 instance_dict['RootDeviceType'] = instance['RootDeviceType']
-                instance_dict['SecurityGroups'] = '\n'.join(map(str,instance['SecurityGroups']))
+                instance_dict['SecurityGroups'] = '\n'.join(map(str, instance['SecurityGroups']))
                 instance_dict['SourceDestCheck'] = instance.get('SourceDestCheck', None)
                 instance_dict['SpotInstanceRequestId'] = instance.get('SpotInstanceRequestId', None)
                 instance_dict['SriovNetSupport'] = instance.get('SriovNetSupport', None)
                 instance_dict['StateReason'] = instance.get('StateReason', {}).get('Message', None)
-                instance_dict['Tags'] = instance_tags(instance.get('Tags', []))
+                instance_dict['Tags'] = resource_tags(instance.get('Tags', []))
                 instance_dict['VirtualizationType'] = instance['VirtualizationType']
                 instance_dict['CpuOptions'] = instance['CpuOptions']
                 instance_dict['CapacityReservationId'] = instance.get('CapacityReservationId', None)
@@ -109,14 +120,14 @@ for region in regions:
                 instance_dict['TpmSupport'] = instance.get('TpmSupport', None)
                 instance_dict['MaintenanceOptions'] = instance['MaintenanceOptions']['AutoRecovery']
                 instance_dict['CurrentInstanceBootMode'] = instance.get('CurrentInstanceBootMode', None)
-
+                # Append instance details to the list
+                instances_list.append(instance_dict)
+                # Print instance ID
                 print(instance['InstanceId'])
 
-                instances_list.append(instance_dict)
-            
 # Create a DataFrame using pandas
 df = pd.DataFrame(instances_list)
 
 # Write the DataFrame to a CSV file
-csv_filename = 'describe_instances.csv'
-df.to_csv(csv_filename, index=False)
+filename = 'describe_instances.csv'
+df.to_csv(os.path.join(directory, filename), index=False)
